2c99825be3f342c66d50301152c9cf9d76d9436e
Lunetistul1976 <raresv03@gmail.com>
2023-01-19 15:26:14 +0200

    This are the latest committed changes

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 <footer>
     <img src="restaurant.jpg"></img>
-    <>Doormat navigation</>
+    <article>Doormat navigation</article>
     <ul className="doormat">
     <li><a href="/home">Home</a></li>        
     <li><a href="/about">About </a></li>
@@ -12,17 +12,17 @@
 
     <ul className="contact">
     <li><a href="/contact.me">Contact</a></li>        
-    <>
+    <article>
     Adress
     Phone number
-    Email </>
+    Email </article>
     </ul>
     
     <ul className="socialmedia">
         <li><a href="/socialmedia">Social media links</a></li>
-        <>
+        <article>
  Adress
  Phone number
- Email</>
+ Email</article>
     </ul>
     </footer>
--- src/Header.js
+++ src/Header.js
@@ -1,3 +1,3 @@
 <header>
-<img src="Logo.svg"></img> 
+<h1><img src="Logo.svg"></img></h1> 
 </header>
